c7b7ea3cf6470c7a48cf5f2865b285edbbc10759
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-08-10 17:24:36 -0600

    modifying IntreviewList and stories

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,24 @@
 import React from "react";
 import classNames from "classnames";
 import "components/InterviewerList.scss"
+import InterviewerListItem from "components/InterviewerListItem";
 
 function InterviewerList(props) {
+  const intr = props.interviewers.map(interviewer=>{
+    return(
+      <InterviewerListItem
+        key={interviewer.id}
+        name = {interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.interviewer}
+      />
+    );
+  })
   return(
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{intr}</ul>
     </section>
   );
 }
