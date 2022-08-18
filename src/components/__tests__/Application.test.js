132faecbf25ebec2cc4e10d4a90d9e012c75e805
vagrant <eng.saad.najm@gmail.com>
2022-08-18 16:47:35 +0000

    End to End testing with Cypress and mock test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,11 +11,13 @@ import {
   getByAltText,
   getByPlaceholderText,
   toBeInTheDocument,
-  queryByText
+  queryByText,
+  queryByAltText
 
 } from "@testing-library/react";
 
 import Application from "components/Application";
+import axios from "axios";
 
 afterEach(cleanup);
 
@@ -57,4 +59,71 @@ describe("Application", () => {
     expect(getByText(day, "3 spots remaining")).toBeInTheDocument();
   });
 
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(
+      container,
+      "appointment"
+    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    //4. Check that the confirmation message is shown.
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(queryByText(appointment, "Confirm"));
+    //6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    // 7. Wait until the element with the text "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Add"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+  expect(getByText(day, "5 spots remaining")).toBeInTheDocument();
+});
+
+// test 3
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment").find((appointment) => queryByText(appointment, "Archie Cohen"));
+
+    // With the existing interview we want to find the edit button.
+    fireEvent.click(getByAltText(appointment, "Edit"));
+    // We change the name and save the interview.
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia test" },
+    });
+    // We don't want the spots to change for "Monday", since this is an edit.
+    // Read the errors because sometimes they say that await cannot be outside of an async function.
+    //Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
+    //Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    // Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    await waitForElement(() => getByText(appointment, "Lydia test"));
+    //Check that the DayListItem with the text "Monday" also has the text "5 spots remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "4 spots remaining")).toBeInTheDocument();
+    expect(getByText(appointment, "Lydia test")).toBeInTheDocument();
+});
+// test 4
+it("shows the delete error when failing to delete an existing appointment", () => {
+  axios.put.mockRejectedValueOnce();
+});
+// test 5
+it("shows the save error when failing to save an appointment", () => {
+  axios.put.mockRejectedValueOnce();
+});
 });
